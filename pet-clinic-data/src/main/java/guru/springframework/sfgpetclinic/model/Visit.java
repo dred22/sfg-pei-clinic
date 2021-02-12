6bb6e49911dffe7f8ce08735c413e8e1a7854160
dred22 <TeLegraph83>
2021-02-12 10:35:45 +0100

    Added visit entity. Closes #38

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -1,11 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
     private LocalDate date;
     private String prescription;
+    @ManyToOne
+    @JoinColumn(name = "pet_it")
     private Pet pet;
 
     public LocalDate getDate() {
