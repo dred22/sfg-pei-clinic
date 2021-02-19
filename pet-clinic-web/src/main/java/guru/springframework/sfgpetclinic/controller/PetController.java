ccec58b83b2a4032edfb6461ead3c2769021dedb
dred22 <TeLegraph83>
2021-02-19 11:26:10 +0100

    Added model attribute for Pet controller. Closes #56

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/PetController.java
@@ -0,0 +1,47 @@
+package guru.springframework.sfgpetclinic.controller;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.service.OwnerService;
+import guru.springframework.sfgpetclinic.service.PetService;
+import guru.springframework.sfgpetclinic.service.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
+class PetController {
+
+    private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
+
+    private final PetService pets;
+    private final PetTypeService petTypeService;
+    private final OwnerService owners;
+
+    public PetController(PetService pets, PetTypeService petTypeService, OwnerService owners) {
+        this.pets = pets;
+        this.petTypeService = petTypeService;
+        this.owners = owners;
+    }
+
+    @ModelAttribute("types")
+    public Set<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable Long ownerId) {
+        return this.owners.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+}
